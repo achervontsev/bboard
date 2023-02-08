7c9813aed5ff51019f88199838ca435ed39db550
Artem Chervontsev <artyom.chervontsev@gmail.com>
2023-02-08 16:37:55 +0300

    Create a user model and login/logout views
    Create a url path and a view to display information pages, such as
    'about', that takes its template name as a page id. Create a
    template for the 'about' page.
    Create a model for users that inherits from the AbstractUser model.
    Register it in the app's admin.py.
    Create views for users to log-in and logout, that inherit from the
    LoginView and LogoutView authentications views. Also create a view
    that displays a user's profie. Create url paths and templates for
    all these views.

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,9 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/logout/', views.BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('accounts/login/', views.BBLoginView.as_view(), name='login'),
+    path('<str:page>/', views.other_page, name='other'),
     path('', views.index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LogoutView, LoginRequiredMixin):
+    template_name = 'main/logout.html'
+    
